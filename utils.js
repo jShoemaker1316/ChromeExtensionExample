ca7fb9d7f01001d6df019b3bb6024fc38040d38a
JShoemaker1316 <jonathanshoemakerdev@gmail.com>
2022-08-09 17:27:29 -0500

    finished project being uploaded

---
+++ utils.js
@@ -0,0 +1,12 @@
+// tutorial code was wrong, but this helped:
+// https://stackoverflow.com/questions/6132018/how-can-i-get-the-current-tab-url-for-chrome-extension
+
+export async function getActiveTabURL() {
+    const tabs = await chrome.tabs.query({
+        currentWindow: true,
+        active: true
+    });
+  
+    return tabs[0];
+}
+
